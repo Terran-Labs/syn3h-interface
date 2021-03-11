c7bb9ea4501980f02627af5da3f293e3aa99fd16
Aubrey Falconer <a@aubreyfalconer.com>
2021-03-10 20:03:27 -0800

    Installed redux-zero.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,6 +1,8 @@
 import { FunctionalComponent, h } from 'preact';
 import { Route, Router } from 'preact-router';
+import { Provider } from "redux-zero/preact";
 
+import store from "./appStore";
 import Home from '../routes/home';
 import Profile from '../routes/profile';
 import NotFoundPage from '../routes/notfound';
@@ -9,13 +11,15 @@ import Header from './header';
 const App: FunctionalComponent = () => {
     return (
         <div id="app">
-            <Header />
-            <Router>
-                <Route path="/" component={Home} />
-                <Route path="/profile/" component={Profile} user="me" />
-                <Route path="/profile/:user" component={Profile} />
-                <NotFoundPage default />
-            </Router>
+            <Provider store={store}>
+                <Header />
+                <Router>
+                    <Route path="/" component={Home} />
+                    <Route path="/profile/" component={Profile} user="me" />
+                    <Route path="/profile/:user" component={Profile} />
+                    <NotFoundPage default />
+                </Router>
+            </Provider>
         </div>
     );
 };
